8498315f1f87119a3956c3f243e969392b85d9c6
Fernando <fernandoguarany@outlook.com>
2021-09-11 10:53:16 -0300

    Atualizacao projeto 2

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -8,6 +8,12 @@ public class CriaConta {
 		
 		primeiraConta.saldo += 100;
 		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		
+		System.out.println("Primeira conta tem " + primeiraConta.saldo);
+		System.out.println("Segunda conta tem " + segundaConta.saldo);
 
 	}
 
